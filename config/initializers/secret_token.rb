be1b2ce7dd2161f2552a443e8ce353a07e90a1e9
Daniel Warner Smith <sendto.dws@gmail.com>
2013-06-12 12:53:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '413b18d1c0aa9e1134a2332d2575666bff3dd826e15018adc02a168caf72f9eac827b3f1b9572e5a4487d7830e0959b920c749fa680d5fcbab1710ff2896d7af'
